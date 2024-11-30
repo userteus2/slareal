bd89c16bf2ec9ace69e36880f4283d0a6852dc71
userteus2 <mateusscordeiro779@gmail.com>
2024-11-30 15:57:48 -0300

    Update sla.lua

--- sla.lua
+++ sla.lua
@@ -2706,17 +2706,35 @@ local L4 = Instance.new("ImageLabel")
 local sound = Instance.new("Sound")
 L3.Name = "UICorner"
 L3.Parent = L2
-L4.Name = "CustomButton"
+L4.Name = "ButtonImage"
 L4.Parent = L2
 L4.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
 L4.BackgroundTransparency = 1.000
 L4.BorderSizePixel = 0
-L4.Position = UDim2.new(0.1208, 0, 0.0953, 0) 
-L4.Size = UDim2.new(0, 50, 0, 50)
-L4.Image = "rbxassetid://111005674884784"
+L4.Position = UDim2.new(0.1, 0, 0.1, 0) 
+L4.Size = UDim2.new(0, 45, 0, 45)
+L4.Image = ""
 L1.Name = "MainGui"
 L1.Parent = player:WaitForChild("PlayerGui")
 L1.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
+L2.Name = "CustomButton"
+L2.Parent = L1
+L2.BackgroundColor3 = Color3.fromRGB(30, 30, 30)
+L2.BackgroundTransparency = 0.2
+L2.BorderSizePixel = 0
+L2.Position = UDim2.new(0.1208, 0, 0.0953, 0)
+L2.Size = UDim2.new(0, 50, 0, 50)
+L2.Image = "rbxassetid://111005674884784"
+L2.Font = Enum.Font.LuckiestGuy
+L2.Text = "Teus Hub"
+L2.TextColor3 = Color3.fromRGB(255, 255, 255)
+L2.TextSize = 9.000
+L2.Draggable = true
+sound.Parent = L2
+sound.SoundId = "rbxassetid://130785805"
+L2.MouseButton1Click:Connect(function()
+    game:GetService("VirtualInputManager"):SendKeyEvent(true, Enum.KeyCode.LeftControl, false, game)
+    sound:Play()
 end)
 print("stop tween") 
 local ScreenGui = Instance.new("ScreenGui")
