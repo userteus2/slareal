e783aa4c036687e7a2902ffa1fa275c41d42526b
userteus2 <mateusscordeiro779@gmail.com>
2024-12-10 04:12:53 -0300

    Update sla.lua

--- sla.lua
+++ sla.lua
@@ -2189,7 +2189,7 @@ if _G.Switch_Hub_Series_R then
 				CFrameQ = CFrame.new(1059.37195, 15.4495068, 1550.4231, 0.939700544, -0, -0.341998369, 0, 1, -0, 0.341998369, 0, 0.939700544)
 				CFrameMon = CFrame.new(1353.44885, 3.40935516, 1376.92029, 0.776053488, -6.97791975e-08, 0.630666852, 6.99138596e-08, 1, 2.4612488e-08, -0.630666852, 2.49917598e-08, 0.776053488)
 				Next_Level_X = 10
-			elseif game.Players.LocalPlayer.Data.Level.Value == 10 or game.Players.LocalPlayer.Data.Level.Value <= 100 then
+			elseif game.Players.LocalPlayer.Data.Level.Value == 10 or game.Players.LocalPlayer.Data.Level.Value <= 50 then
 				Ms = "Shanda"
 				NameQuest = "SkyExp1Quest"
 				QuestLv = 2
