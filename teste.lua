6b3b6f037466e085f8b5b650d2f9644344d08f4c
userteus2 <mateusscordeiro779@gmail.com>
2025-03-06 21:48:53 -0300

    Update teste.lua

--- teste.lua
+++ teste.lua
@@ -1,7 +1,22 @@
-_G.whiteScreen = false
-_G.fps = 70
-_G.Mode = false
-loadstring(game:HttpGet('https://raw.githubusercontent.com/AloneBiNgu/AloneHub/main/lag'))()
+for i,v in next, workspace:GetDescendants() do
+    pcall(function()
+        v.Transparency = 1
+    end)
+end
+for i,v in next, getnilinstances() do
+    pcall(function()
+        v.Transparency = 1
+        for i1,v1 in next, v:GetDescendants() do
+            v1.Transparency = 1
+        end
+    end)
+end
+a = workspace
+a.DescendantAdded:Connect(function(v)
+    pcall(function()
+        v.Transparency = 1
+    end)
+end)
 
 local RunService = game:GetService("RunService")
 local LocalPlayer = game.Players.LocalPlayer
