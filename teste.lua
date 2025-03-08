1170434d458bca45ba2b402f5a35e3120c8fe0f1
userteus2 <mateusscordeiro779@gmail.com>
2025-03-08 01:30:11 -0300

    Update teste.lua

--- teste.lua
+++ teste.lua
@@ -1,29 +1,17 @@
 local player = game.Players.LocalPlayer
 local playerCharacter = player.Character or player.CharacterAdded:Wait()
 
--- Função para definir transparência, ignorando o personagem do jogador
 local function setTransparency(object)
     if not object:IsDescendantOf(playerCharacter) and object:IsA("BasePart") then
-        pcall(function()
-            object.Transparency = 1
-        end)
+        pcall(function() object.Transparency = 1 end)
     end
 end
 
--- Aplica transparência a todos os objetos no workspace, exceto o personagem do jogador
-for _, v in ipairs(workspace:GetDescendants()) do
-    setTransparency(v)
-end
+for _, v in ipairs(workspace:GetDescendants()) do setTransparency(v) end
 
--- Monitora objetos adicionados ao workspace e aplica a transparência, exceto ao personagem do jogador
-workspace.DescendantAdded:Connect(function(v)
-    setTransparency(v)
-end)
+workspace.DescendantAdded:Connect(setTransparency)
 
--- Atualiza a referência do personagem quando o jogador reseta
-player.CharacterAdded:Connect(function(newCharacter)
-    playerCharacter = newCharacter -- Atualiza para o novo personagem do jogador
-end)
+player.CharacterAdded:Connect(function(newCharacter) playerCharacter = newCharacter end)
 
 -- Max level, godhuman, cdk, sgt
 script_key = "U3VLB2EKRGQJJCVX433PZM5HXGYA" -- premium only, u can leave it blank if ur not
