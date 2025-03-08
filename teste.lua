121de94a50f5279e916ca9f6fa30d56c3354cd21
userteus2 <mateusscordeiro779@gmail.com>
2025-03-08 01:46:27 -0300

    Update teste.lua

--- teste.lua
+++ teste.lua
@@ -1,5 +1,23 @@
 -- Delete map
-loadstring(game:HttpGet("https://raw.githubusercontent.com/userteus2/slareal/refs/heads/main/sla.lua"))()
+for i,v in next, workspace:GetDescendants() do
+    pcall(function()
+        v.Transparency = 1
+    end)
+end
+for i,v in next, getnilinstances() do
+    pcall(function()
+        v.Transparency = 1
+        for i1,v1 in next, v:GetDescendants() do
+            v1.Transparency = 1
+        end
+    end)
+end
+a = workspace
+a.DescendantAdded:Connect(function(v)
+    pcall(function()
+        v.Transparency = 1
+    end)
+end)
 
 -- Delete Misc
 local CoreGui = game:GetService("StarterGui")
